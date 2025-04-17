6ea9989d83419dd2657e9c5765690e850f325124
Juan Manuel González Espinosa <jm.ge.esp@gmail.com>
2025-04-17 16:51:47 -0600

    🔄 Migración a MongoDB Atlas: Actualizada conexión para usar URI remoto

--- conexion.py
+++ conexion.py
@@ -1,20 +1,7 @@
 from pymongo import MongoClient
-from pymongo.errors import ConnectionFailure
-
-usuario = "admin"
-password = "cajero1234"
-host = "187.190.159.78"
-puerto = 27017
-base_datos = "banco_db"
-
-uri = f"mongodb://{usuario}:{password}@{host}:{puerto}/?authSource=admin"
 
 def obtener_conexion():
-    try:
-        client = MongoClient(uri)
-        client.admin.command('ping')  # Verifica la conexión
-        db = client[base_datos]
-        return db
-    except ConnectionFailure as e:
-        print("Error de conexión:", e)
-        return None
+    uri = "mongodb+srv://admin:cajero1234@cluster0.ndweyrc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    cliente = MongoClient(uri)
+    db = cliente["banco_db"]  # Cambia "banco_db" por el nombre real de tu base
+    return db
