90b5db16e043aa1cb05ad46e3a28f23527e145de
Juan Manuel González Espinosa <jm.ge.esp@gmail.com>
2025-04-17 17:41:09 -0600

    🔄 Migración a MongoDB Atlas: Actualizada conexión para usar URI remoto

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def index():
     <html lang="es">
     <head>
         <meta charset="UTF-8">
-        <title>Cajero Thunder</title>
+        <title>Cajero Thunder prueba</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
         <!-- ========== SECCIÓN CSS ========== -->
