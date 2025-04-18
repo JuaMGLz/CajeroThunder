a13995796491bfa5a7b0a042e71f127a72e53a8c
Juan Manuel González Espinosa <jm.ge.esp@gmail.com>
2025-04-17 23:33:00 -0600

    caray señor barriga no se hubiera molestado

--- conexion.py
+++ conexion.py
@@ -1,6 +1,12 @@
 from pymongo import MongoClient
+import certifi
 
 def obtener_conexion():
-    uri = "mongodb+srv://admin:cajero1234@cluster0.ndweyrc.mongodb.net/banco_db?retryWrites=true&w=majority"
-    cliente = MongoClient(uri)
-    return cliente
+    uri = "mongodb+srv://admin:cajero1234@cluster0.ndweyrc.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsCAFile=" + certifi.where()
+    client = MongoClient(uri)
+    db = client["banco_db"]
+    return db
+
+
+import certifi
+print("Certifi path en Render:", certifi.where())
