5bd0c2b7a1f8d8214a7d0292fdb8212f3667080c
Juan Manuel González Espinosa <jm.ge.esp@gmail.com>
2025-04-17 18:01:37 -0600

    🔄 Migración a MongoDB Atlas y ajustes finales para Render

--- conexion.py
+++ conexion.py
@@ -2,6 +2,6 @@ from pymongo import MongoClient
 
 def obtener_conexion():
     uri = "mongodb+srv://admin:cajero1234@cluster0.ndweyrc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    cliente = MongoClient(uri)
-    db = cliente["banco_db"]  # Cambia "banco_db" por el nombre real de tu base
+    cliente = MongoClient(uri, serverSelectionTimeoutMS=5000)
+    db = cliente["banco_db"]
     return db
