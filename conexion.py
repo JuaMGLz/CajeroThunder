d40a16d0b5f560f1a1cb378f97e3470ecf1ad7aa
Juan Manuel González Espinosa <jm.ge.esp@gmail.com>
2025-04-17 18:45:47 -0600

    optimista

--- conexion.py
+++ conexion.py
@@ -2,7 +2,11 @@ from pymongo import MongoClient
 import certifi
 
 def obtener_conexion():
+    print("Ubicación del certificado:", certifi.where())  # esto lo verás en los logs de Render
     uri = "mongodb+srv://admin:cajero1234@cluster0.ndweyrc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     cliente = MongoClient(uri, tlsCAFile=certifi.where())
     db = cliente["banco_db"]
     return db
+
+import certifi
+print("Certifi path en Render:", certifi.where())
