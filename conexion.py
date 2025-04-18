7eb12adeab3d13a6be777dc1ec449ab560ac7678
Juan Manuel González Espinosa <jm.ge.esp@gmail.com>
2025-04-17 23:30:58 -0600

    creo que ya

--- conexion.py
+++ conexion.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
 def obtener_conexion():
-    client = MongoClient("mongodb://admin:cajero1234@localhost:27017/?authSource=admin")
-    db = client["banco_db"]
-    return db
+    uri = "mongodb+srv://admin:cajero1234@cluster0.ndweyrc.mongodb.net/banco_db?retryWrites=true&w=majority"
+    cliente = MongoClient(uri)
+    return cliente
