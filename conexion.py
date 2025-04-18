6380af612a38a2b0eaefe51664eefba7692c0e39
Juan Manuel González Espinosa <jm.ge.esp@gmail.com>
2025-04-17 18:32:02 -0600

    dice que así

--- conexion.py
+++ conexion.py
@@ -1,14 +1,8 @@
 from pymongo import MongoClient
+import certifi
 
 def obtener_conexion():
-    uri = "mongodb://admin:cajero1234@ac-vphjntx-shard-00-00.ndweyrc.mongodb.net:27017,ac-vphjntx-shard-00-01.ndweyrc.mongodb.net:27017,ac-vphjntx-shard-00-02.ndweyrc.mongodb.net:27017/banco_db?ssl=true&replicaSet=atlas-tdwl2z-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
-    cliente = MongoClient(uri, serverSelectionTimeoutMS=5000)
-
-    try:
-        cliente.admin.command('ping')
-        print("✅ Conexión exitosa con MongoDB Atlas")
-    except Exception as e:
-        print("❌ Error en conexión con MongoDB Atlas:", e)
-
+    uri = "mongodb+srv://admin:cajero1234@cluster0.ndweyrc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    cliente = MongoClient(uri, tlsCAFile=certifi.where())
     db = cliente["banco_db"]
     return db
