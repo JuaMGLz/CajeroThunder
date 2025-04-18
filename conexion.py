94107084d447a87f802a7f4d1fcc077420e83fad
Juan Manuel González Espinosa <jm.ge.esp@gmail.com>
2025-04-17 23:09:18 -0600

    ya merito

--- conexion.py
+++ conexion.py
@@ -4,3 +4,10 @@ def obtener_conexion():
     uri = "mongodb+srv://admin:cajero1234@cluster0.ndweyrc.mongodb.net/banco_db?retryWrites=true&w=majority"
     cliente = MongoClient(uri)
     return cliente
+
+import os
+from pymongo import MongoClient
+
+uri = os.getenv("MONGO_URI")
+cliente = MongoClient(uri)
+db = cliente["banco_db"]
