3681a3716fcc13d5c6a84a2d9e9a4c4817826f75
Juan Manuel González Espinosa <jm.ge.esp@gmail.com>
2025-04-17 18:27:49 -0600

    viendo que fue parte 2

--- conexion.py
+++ conexion.py
@@ -1,10 +1,14 @@
+from pymongo import MongoClient
+
 def obtener_conexion():
-    from pymongo import MongoClient
-    uri = "mongodb+srv://admin:cajero1234@cluster0.ndweyrc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    uri = "mongodb://admin:cajero1234@ac-vphjntx-shard-00-00.ndweyrc.mongodb.net:27017,ac-vphjntx-shard-00-01.ndweyrc.mongodb.net:27017,ac-vphjntx-shard-00-02.ndweyrc.mongodb.net:27017/banco_db?ssl=true&replicaSet=atlas-tdwl2z-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+    cliente = MongoClient(uri, serverSelectionTimeoutMS=5000)
+
     try:
-        cliente = MongoClient(uri, serverSelectionTimeoutMS=5000)
-        cliente.admin.command('ping')  # Forzar prueba de conexión
-        return cliente["banco_db"]
+        cliente.admin.command('ping')
+        print("✅ Conexión exitosa con MongoDB Atlas")
     except Exception as e:
-        print("❌ Error de conexión:", e)
-        return None
+        print("❌ Error en conexión con MongoDB Atlas:", e)
+
+    db = cliente["banco_db"]
+    return db
