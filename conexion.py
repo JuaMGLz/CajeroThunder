4eaf1b55d984253dadcee36c33bf5c6a0c1604da
Juan Manuel González Espinosa <jm.ge.esp@gmail.com>
2025-04-17 21:46:25 -0600

    cambiando ando

--- conexion.py
+++ conexion.py
@@ -1,14 +1,12 @@
 from pymongo import MongoClient
-import os
 import certifi
-from pymongo.ssl_support import get_ssl_context
 
 def obtener_conexion():
-    uri = os.getenv("MONGO_URI")  # mejor usar variable de entorno
-    if not uri:
-        raise Exception("MONGO_URI no definida")
+    uri = "mongodb+srv://admin:cajero1234@cluster0.ndweyrc.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsCAFile=" + certifi.where()
+    client = MongoClient(uri)
+    db = client["banco_db"]
+    return db
 
-    ssl_context = get_ssl_context(ca_certs=certifi.where())
 
-    client = MongoClient(uri, ssl=True, ssl_cert_reqs='CERT_REQUIRED', ssl_ca_certs=certifi.where())
-    return client["banco_db"]
+import certifi
+print("Certifi path en Render:", certifi.where())
