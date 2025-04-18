decec0aa7ed7ee33908c81bb967d16998f8435bb
Juan Manuel González Espinosa <jm.ge.esp@gmail.com>
2025-04-17 23:44:14 -0600

    otra conexión

--- conexion.py
+++ conexion.py
@@ -1,6 +1,18 @@
 from pymongo import MongoClient
+import os
+
+# Obtenemos la URI desde variables de entorno
+MONGO_URI = os.environ.get("MONGO_URI")
 
 def obtener_conexion():
-    client = MongoClient("mongodb://admin:cajero1234@localhost:27017/?authSource=admin")
+    client = MongoClient(MONGO_URI)
     db = client["banco_db"]
     return db
+
+# Validación al iniciar
+try:
+    client = MongoClient(MONGO_URI)
+    client.admin.command('ping')
+    print("✅ Conexión exitosa a MongoDB Atlas")
+except Exception as e:
+    print("❌ Error de conexión:", e)
