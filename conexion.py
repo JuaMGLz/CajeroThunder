232eb1c98307f427040dc1f9804829111e6463d5
Juan Manuel González Espinosa <jm.ge.esp@gmail.com>
2025-04-17 18:20:27 -0600

    cambiando conexión otra vez

--- conexion.py
+++ conexion.py
@@ -1,7 +1,10 @@
-from pymongo import MongoClient
-
 def obtener_conexion():
+    from pymongo import MongoClient
     uri = "mongodb+srv://admin:cajero1234@cluster0.ndweyrc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    cliente = MongoClient(uri, serverSelectionTimeoutMS=5000)
-    db = cliente["banco_db"]
-    return db
+    try:
+        cliente = MongoClient(uri, serverSelectionTimeoutMS=5000)
+        cliente.admin.command('ping')  # Forzar prueba de conexión
+        return cliente["banco_db"]
+    except Exception as e:
+        print("❌ Error de conexión:", e)
+        return None
